e512c9165a5a53d47b0fb99928b43987939d32ae
Alice Zhao <alicezhao@SHA-MC0326.local>
2020-08-21 14:36:54 +0800

    结合mobx

--- src/mobx-components/Timer.jsx
+++ src/mobx-components/Timer.jsx
@@ -2,23 +2,36 @@ import  React,{useState,useEffect} from 'react';
 import { observable } from 'mobx';
 import {observer}from 'mobx-react'
 
-const Timer=observer(()=>{
-    const [timeData]=useState(()=>
-        observable([{
-            name:"alice",
-            age:12,
-            hobby:[{
-                text:'sing',
-            },{
-                text:'paint'
-            }]
-        }])
-    );
+// const Timer=observer(()=>{
+//     const [timeData]=useState(()=>
+//         observable([{
+//             name:"alice",
+//             age:12,
+//             hobby:[{
+//                 text:'sing',
+//             },{
+//                 text:'paint'
+//             }]
+//         }])
+//     );
 
-    useEffect(() => {
-        timeData[0].hobby[0].text='hhhh'
-    }, []);
-    return <span>{timeData[0].hobby[0].text}</span>
-})
+//     useEffect(() => {
+//         timeData[0].hobby[0].text='hhhh'
+//     }, []);
+//     return <span>{timeData[0].hobby[0].text}</span>
+// })
+
+@observer
+class Timer extends React.Component{
+    @observable timeShow=0;
+    componentWillMount(){
+        setInterval(()=>{
+            this.timeShow++
+        },1000)
+    }
+    render(){
+        return <span>{this.timeShow}</span>
+    }
+}
 
 export default Timer;
